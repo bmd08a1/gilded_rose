22dc139dafaf3c58594e21a2667e781d06dcdfbc
Dung Bui <dung.bui@saleshood.com>
2018-09-26 17:55:09 +0700

    update_quality will change the quality attribute only. quality_degradation_level will calculate the number of units needed to be change. quality_degradation_level function don't need to be tested because it is covered by update_quality test cases

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,50 +4,15 @@ def update_quality(items)
   items.each do |item|
     next if item.name == 'Sulfuras, Hand of Ragnaros'
 
-    case item.name
-    when 'Aged Brie'
-      if item.quality < MAX_QUALITY
-        item.quality += UNIT
-      end
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality < MAX_QUALITY
-        item.quality += UNIT
-        if item.sell_in < 11
-          item.quality += UNIT
-        end
-        if item.sell_in < 6
-          item.quality += UNIT
-        end
-      end
-    when 'Conjured'
-      if item.quality > MIN_QUALITY
-        item.quality -= 2*UNIT
-      end
-    else
-      if item.quality > MIN_QUALITY
-        item.quality -= UNIT
-      end
-    end
-
     decrease_sell_in item
 
-    if item.sell_in < 0
-      case item.name
-      when 'Aged Brie'
-        if item.quality < MAX_QUALITY
-          item.quality += UNIT
-        end
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality = MIN_QUALITY
-      when 'Conjured'
-        if item.quality > MIN_QUALITY
-          item.quality -= 2*UNIT
-        end
-      else
-        if item.quality > MIN_QUALITY
-          item.quality -= UNIT
-        end
-      end
+    quality_degradation_level = quality_degradation_level(item)
+    updated_quality = item.quality - quality_degradation_level*UNIT
+
+    if quality_degradation_level > 0
+      item.quality = [updated_quality, MIN_QUALITY].max
+    else
+      item.quality = [updated_quality, MAX_QUALITY].min
     end
   end
 end
@@ -55,6 +20,39 @@ end
 def decrease_sell_in item
   return if item.name == 'Sulfuras, Hand of Ragnaros'
   item.sell_in -= 1
+end
+
+def quality_degradation_level(item)
+  case item.name
+  when 'Aged Brie'
+    if item.sell_in > 0
+      return -1
+    else
+      return -2
+    end
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    if item.sell_in >= 10
+      return -1
+    elsif item.sell_in >= 5
+      return -2
+    elsif item.sell_in >= 0
+      return -3
+    else
+      return item.quality
+    end
+  when 'Conjured'
+    if item.sell_in > 0
+      return 2
+    else
+      return 4
+    end
+  else
+    if item.sell_in > 0
+      return 1
+    else
+      return 2
+    end
+  end
 end
 
 ######### DO NOT CHANGE BELOW #########
